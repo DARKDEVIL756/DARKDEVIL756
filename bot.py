2fcd116da146d12128bc22b7659bc3fd82a2bd78
DARK DEVIL <101184455+DARKDEVIL756@users.noreply.github.com>
2022-05-25 20:32:11 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,12 +5,4 @@ client = discord.client()
 async def on_ready():
     print('we have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):    
-if message.author == client.user:
-    return
-
-if message.content.startwith('$hi'):
-    await message.channel.send('Hello!') 
-
 client.run('OTc4OTczNjU5MDM1MjIyMDI2.Gqjswo.uRYYgKIcGYyYNafWn0h4iJTcNhFIAIvmt8KGnM')  
