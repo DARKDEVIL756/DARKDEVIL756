87a52b509c2ccfa3b34c6d57307ba3c9438ccfc9
DARK DEVIL <101184455+DARKDEVIL756@users.noreply.github.com>
2022-05-26 03:17:41 +0600

    Delete bot.py

--- bot.py
+++
@@ -1,8 +0,0 @@
-import discord
-client = discord.client()
-
-@client.event
-async def on_ready():
-    print('we have logged in as {0.user}'.format(client))
-
-client.run('OTc4OTczNjU5MDM1MjIyMDI2.Gqjswo.uRYYgKIcGYyYNafWn0h4iJTcNhFIAIvmt8KGnM')  
