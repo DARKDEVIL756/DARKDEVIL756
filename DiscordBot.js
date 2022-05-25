8406d5bbcebea30a6da5fb954ab2216fc25e099e
DARK DEVIL <101184455+DARKDEVIL756@users.noreply.github.com>
2022-05-25 22:48:34 +0600

    Update DiscordBot.js

--- DiscordBot.js
+++ DiscordBot.js
@@ -1,9 +1,11 @@
 const Discord = require('discord.js');
 const bot = new Discord.client();
-const token = '';
+const token = 'OTc4OTczNjU5MDM1MjIyMDI2.GpN47t.79AwAkVB7SHhik4DLMZbMKZwa1siZ2ABiHC02I';
 const PEEFIX = '';
 
 
-bot.on('ready') () => {
+bot.on('ready', () => {
     console.log('i am online!')
+
 })
+bot.loging(token)
