a1581550d35da0a9038264c32d918cffd9a352a7
DARK DEVIL <101184455+DARKDEVIL756@users.noreply.github.com>
2022-05-25 17:57:42 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,17 @@
 import discord
+
+client = discord.client()
+
+@client.event
+async def on_ready():
+    print('we have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message): 
+if message.author == client.user:
+    return
+
+if message.content.startwith('$hi'):
+    await message.channel.send('Hello!') 
+
+client.run(OTc4OTczNjU5MDM1MjIyMDI2.Gqjswo.uRYYgKIcGYyYNafWn0h4iJTcNhFIAIvmt8KGnM)       
